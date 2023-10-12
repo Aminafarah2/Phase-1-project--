70219d1264e2a6d459373a856f36d0a41e04a403
aminafarah <amishkamohamed563@gmail.com>
2023-10-12 16:20:36 +0300

    done with css

---
+++ index.js
@@ -0,0 +1,32 @@
+const dogImage = document.getElementById("dogImage");
+const fetchButton = document.getElementById("btn");
+
+// Function to fetch a random dog image
+function fetchRandomDogImage() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "success") {
+                // Set the image source to the fetched dog image URL
+                dogImage.src = data.message;
+            } else {
+                alert("Failed to fetch dog image.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching dog image:", error);
+        });
+}
+fetchRandomDogImage();
+// Event listeners
+fetchButton.addEventListener("click", fetchRandomDogImage);
+
+dogImage.addEventListener("load", () => {
+    // Adjust the container's size when the image loads
+    document.getElementById("container").style.width = dogImage.width + "px";
+    document.getElementById("container").style.height = dogImage.height + "px";
+});
+
+dogImage.addEventListener("error", () => {
+    alert("Failed to load dog image.");
+});
